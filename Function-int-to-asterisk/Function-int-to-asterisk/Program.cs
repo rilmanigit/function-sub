838a2a1606ba248d3a2453444a33f74c1eb34f69
rilmanigit <risto.ilmaniemi@student.saimia.fi>
2017-03-03 19:13:41 +0200

    New Commit

---
+++ Function-int-to-asterisk/Function-int-to-asterisk/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function_int_to_asterisk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program asks a number (an integer) and will print given number of asterisks on a screen
+            //Negative numbers are not allowed and zero will result an announcement of entered zero
+            int i;
+            int n;
+            Console.Write("Please enter a number (an integer): ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n < 0)
+            {
+                Console.WriteLine("Negative number {0} is not allowed number.", n);
+            }
+
+            else if (n == 0)
+            {
+                Console.WriteLine("You have given {0}, no asterisks printed.", n);
+            }
+
+            for (i = 1; i <= n; ++i)
+            {
+                Console.Write("*");
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Minimum-number/func-minimum-number/func-minimum-number/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace func_minimum_number
+
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nums;
+            int num1, num2;
+            Console.WriteLine("Please enter two numbers separated by comma (eg. 2,4): ");
+            nums = Console.ReadLine();
+            num1 = Convert.ToInt32(nums.Split(',')[0]);
+            num2 = Convert.ToInt32(nums.Split(',')[1]);
+            int result = Result(num1, num2);
+            Console.WriteLine("\nNumber " + result + " is minimum.");
+            Console.ReadKey();
+        }
+
+
+        public static int Result(int num1, int num2)
+        {
+            if (num1 < num2)
+            {
+                return num1;
+            }
+
+            else
+            {
+                return num2;
+            }
+
+        }
+
+
+
+    }
+}
